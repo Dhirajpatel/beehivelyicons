e9fb440de78e39df01644ab985fcda732083a276
Dhiraj <dhiraj@insightmethods.com>
2024-02-15 12:41:00 +0530

    create

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import * as Icon from 'beehively-icons';
+import clipboardCopy from 'clipboard-copy';
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [iconList, setIconList] = useState([]);
+  const [copiedIcon, setCopiedIcon] = useState(null);
+
+  useEffect(() => {
+    const icons = Object.keys(Icon);
+    setIconList(icons);
+  }, []);
+
+  const filteredIcons = iconList.filter(icon => icon.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  const handleCopyClick = (icon) => {
+    clipboardCopy(icon)
+      .then(() => {
+        setCopiedIcon(icon);
+        setTimeout(() => setCopiedIcon(null), 1500); // Reset copiedIcon after 1.5 seconds
+      })
+      .catch((err) => console.error('Failed to copy text: ', err));
+  };
+
+  return (
+    <div className="App">
+      <header className="header">
+        <div>
+          <span className="logo">Beehively Icons</span>
+          <code>
+            v1.0.3
+          </code>
+        </div>
+        <div className='topRight'>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Dhirajpatel/beehively-icons" className="link">GitHub</a>
+          <button aria-label="Activate dark mode" className="iconButton">
+            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <main className='main'>
+        <div className="searchContainer">
+          <input
+            type="text"
+            placeholder="Search icons..."
+            value={searchTerm}
+            className="searchInput"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <Icon.Search size={20} className="searchIcon" />
+        </div>
+        <div className="iconList">
+          {filteredIcons.map((icon, index) => (
+            <div key={index} className="iconItem" onClick={() => handleCopyClick(icon)}>
+              <span className='svgIcon'>
+                {React.createElement(Icon[icon], { size: 32 })}
+              </span>
+              <span className="iconName">{icon}</span>
+              {copiedIcon === icon && <span className="copyText">Copied</span>}
+              <span className="copyIcon">
+                <Icon.Copy size={20} />
+              </span>
+            </div>
+          ))}
+        </div>
+      </main>
+
+
+    </div>
+  );
+}
+
+export default App;
